86ed84ac10ac370dbcf1ebf1487cac289ca148d4
Michael Doyle <michaeldoyle@google.com>
2026-08-03 14:58:37 -0400

    feat(tools): include input and output schemas in exported .prompt files
    Co-authored-by: Chris Ray Gill <15097183+chrisraygill@users.noreply.github.com>

--- genkit-tools/common/src/server/router.ts
+++ genkit-tools/common/src/server/router.ts
@@ -30,7 +30,6 @@ import * as apis from '../types/apis';
 import { CancelActionRequestSchema } from '../types/apis';
 import type { EnvironmentVariable } from '../types/env';
 import * as evals from '../types/eval';
-import type { PromptFrontmatter } from '../types/prompt';
 import {
   PageViewEvent,
   SelectContentEvent,
@@ -39,7 +38,7 @@ import {
   recordRequestEvent,
 } from '../utils/analytics';
 import { toolsPackage } from '../utils/package';
-import { fromMessages } from '../utils/prompt';
+import { toPromptFile } from '../utils/prompt';
 
 const t = initTRPC.create({
   errorFormatter(opts) {
@@ -137,15 +136,7 @@ export const TOOLS_SERVER_ROUTER = (manager: BaseRuntimeManager) =>
     /** Generate a .prompt file from messages and model config. */
     createPrompt: loggedProcedure
       .input(apis.CreatePromptRequestSchema)
-      .mutation(async ({ input }) => {
-        const frontmatter: PromptFrontmatter = {
-          model: input.model.replace('/model/', ''),
-          config: input.config,
-          tools: input.tools?.map((toolDefinition) => toolDefinition.name),
-          use: input.use,
-        };
-        return fromMessages(frontmatter, input.messages);
-      }),
+      .mutation(async ({ input }) => toPromptFile(input)),
 
     /** Retrieves all traces for a given environment (e.g. dev or prod). */
     listTraces: loggedProcedure
